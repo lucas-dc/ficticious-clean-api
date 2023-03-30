03a82bca96ef5051a82dfbbf8839a0a4f6b1db31
lucas <lcsdacunha@gmail.com>
2023-03-30 10:40:04 -0300

    Initial commit for Ficticious Clean API project

---
+++ src/main/java/br/com/ficticious_clean/api/FicticiousCleanApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ficticious_clean.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicticiousCleanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicticiousCleanApiApplication.class, args);
+	}
+
+}
